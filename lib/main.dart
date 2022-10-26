0158bea93d28289ac83a53aaf73630787785580e
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-10-26 20:47:02 +0700

    update components - assign screens and bottom navigation bar in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:etiket_mobile/screens/bottom_nav.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,46 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
-      home: const MyHomePage(title: 'E-tiket'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'Soon to be E-tiket',
-            ),
-          ],
-        ),
-      ),
+      home: const BottomBar()
     );
   }
 }
