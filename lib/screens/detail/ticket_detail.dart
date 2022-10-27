19660f4ead51e5b84e499827b1b95e9b776b06e1
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-10-27 13:09:56 +0700

    add components - new screens

---
+++ lib/screens/detail/ticket_detail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class TicketDetail extends StatefulWidget {
+  const TicketDetail({Key? key}) : super(key: key);
+
+  @override
+  State<TicketDetail> createState() => _TicketDetailState();
+}
+
+class _TicketDetailState extends State<TicketDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
