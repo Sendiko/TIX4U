c41458bf20a7936f446f8846aa7a60e635677545
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-10-30 14:26:35 +0700

    update components - start making homepage ui

--- lib/screens/main/home_screen.dart
+++ lib/screens/main/home_screen.dart
@@ -1,11 +1,14 @@
+import 'package:etiket_mobile/screens/component/header_image.dart';
+import 'package:etiket_mobile/screens/utils/styles.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({
     super.key,
-    required this.text
+    required this.greeting
   });
-  final String text;
+  final String greeting;
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -14,8 +17,39 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(widget.text),
+    return Scaffold(
+      backgroundColor: Styles.primaryColor,
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.only(left: 18, top: 24, right: 18),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget.greeting,
+                          style: Styles.headlineStyle3,
+                        ),
+                        Text(
+                          "Book Tickets",
+                          style: Styles.headlineStyle2,
+                        ),
+                      ],
+                    ),
+                    const HeaderImage(size: 48)
+                  ],
+                ),
+              ]
+            )
+          )
+        ],
+      ),
     );
   }
 }
