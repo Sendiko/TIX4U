1dbae76128d7530505f017f73e17add63ef73a2d
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-10-30 14:26:09 +0700

    update components - remove debug label, change color

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:etiket_mobile/screens/bottom_nav.dart';
+import 'package:etiket_mobile/screens/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,9 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.indigo,
+        useMaterial3: true,
+        primaryColor: Styles.primaryColor
       ),
       home: const BottomBar()
     );
