fb5175056281061c7841e694c0c1884f22b66cc0
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-10-31 10:54:49 +0700

    update component - change header image size

--- lib/screens/main/home_screen.dart
+++ lib/screens/main/home_screen.dart
@@ -42,7 +42,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                       ],
                     ),
-                    const HeaderImage(size: 48)
+                    const HeaderImage(size: 32)
                   ],
                 ),
               ]
