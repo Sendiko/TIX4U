f809bce598c6d85e690001c2e18364d57ac6851e
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-11-04 17:50:33 +0700

    update components - splash_screen is done

--- lib/screens/welcome/splash_screen.dart
+++ lib/screens/welcome/splash_screen.dart
@@ -1,4 +1,6 @@
+import 'package:etiket_mobile/utils/styles.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -10,6 +12,16 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Styles.primaryColor,
+      body: Center(
+        child:
+        Image.asset(
+          "assets/images/app_icon.png",
+          height: 128,
+          width: 128,
+        )
+      ),
+    );
   }
 }
