295bf6e4b3dd016cc7b47dfa608184c6b619238d
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-11-09 13:41:37 +0700

    update components - extract primary, secondary and tertiary color from colorscheme

--- lib/utils/styles.dart
+++ lib/utils/styles.dart
@@ -1,30 +1,46 @@
 import 'package:flutter/material.dart';
 
 class Styles{
-  static Color primaryColor = const Color(0xFFDBDFFD);
-  static Color onPrimaryColor = const Color(0xFF242F9B);
-  static Color backgroundColor = const Color(0x00ffffff);
+  // primary color
+  static Color primaryColor = const Color(0xFF6750A4);
+  static Color onPrimaryColor = const Color(0xFFFFFFFF);
+  static Color primaryContainerColor = const Color(0xFFEADDFF);
+  static Color onPrimaryContainerColor = const Color(0xFF21005D);
+
+  // secondary color
+  static Color secondaryColor = const Color(0xFF625B71);
+  static Color onSecondaryColor = const Color(0xFFFFFFFF);
+  static Color secondaryContainerColor = const Color(0xFFE8DEF8);
+  static Color onSecondaryContainerColor = const Color(0xFF1D192B);
+
+  // tertiary color
+  static Color tertiaryColor = const Color(0xFF7D5260);
+  static Color onTertiaryColor = const Color(0xFFFFFFFF);
+  static Color tertiaryContainerColor = const Color(0xFFFFD8E4);
+  static Color onTertiaryContainerColor = const Color(0xFF31111D);
+
+  static Color backgroundColor = const Color(0xFFFFFBFE);
   static TextStyle textStyle = TextStyle(
       fontSize: 16,
-      color: onPrimaryColor,
+      color: primaryColor,
       fontFamily: 'Poppins',
       fontWeight: FontWeight.w500
   );
   static TextStyle headlineStyle1 = TextStyle(
       fontSize: 24,
-      color: onPrimaryColor,
+      color: primaryColor,
       fontFamily: 'Poppins',
       fontWeight: FontWeight.bold
   );
   static TextStyle headlineStyle2 = TextStyle(
       fontSize: 20,
-      color: onPrimaryColor,
+      color: primaryColor,
       fontFamily: 'Poppins',
       fontWeight: FontWeight.w600
   );
   static TextStyle headlineStyle3 = TextStyle(
       fontSize: 18,
-      color: onPrimaryColor,
+      color: primaryColor,
       fontFamily: 'Poppins',
       fontWeight: FontWeight.w600
   );
