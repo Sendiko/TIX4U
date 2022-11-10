c085a9b558adc56acee4bed979c509b70ec73b72
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-11-10 13:39:34 +0700

    update components - extract text header

--- lib/screens/main/home_screen.dart
+++ lib/screens/main/home_screen.dart
@@ -1,4 +1,4 @@
-import 'package:etiket_mobile/utils/styles.dart';
+import 'package:etiket_mobile/component/homepage_header.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -23,21 +23,7 @@ class _HomeScreenState extends State<HomeScreen> {
             padding: const EdgeInsets.only(left: 16, top: 32, right: 16),
             child: Column(
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Hi, ${widget.name}!",
-                          style: Styles.headlineStyle2,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                HomepageHeader(name: widget.name)
               ]
             )
           )
