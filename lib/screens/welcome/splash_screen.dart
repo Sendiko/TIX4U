0154f91da170ea047c3ac1d1c33c435a0d4e7aab
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-11-15 14:26:04 +0700

    update component - add timer and navigate

--- lib/screens/welcome/splash_screen.dart
+++ lib/screens/welcome/splash_screen.dart
@@ -1,6 +1,7 @@
+import 'package:etiket_mobile/screens/welcome/welcome_screen.dart';
 import 'package:etiket_mobile/utils/styles.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -11,16 +12,24 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    Timer(
+        const Duration(seconds: 2),
+        () => Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => const WelcomeScreen())));
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Styles.primaryColor,
       body: Center(
-        child: Image.asset(
-          "assets/images/app_icon.png",
-          height: 128,
-          width: 128,
-        )
-      ),
+          child: Image.asset(
+        "assets/images/app_icon.png",
+        height: 128,
+        width: 128,
+      )),
     );
   }
 }
